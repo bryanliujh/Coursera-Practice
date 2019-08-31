ddc15c1165e2806158e2ea01d108f201d20dc4c9
bryanliujh <bbx54ever@hotmail.com>
2019-08-31 19:42:32 +0800

    fast fibonacci

---
+++ src/FastFibonacci.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class FastFibonacci {
+	private static long calc_fib(int n) {
+
+		if (n <= 1) {
+			return n;
+		}
+
+		long[] fibArr = new long[n+1];
+		fibArr[0] = 0;
+		fibArr[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+			fibArr[n] = fibArr[i];
+		}
+
+		return fibArr[n];
+	}
+
+	public static void main(String args[]) {
+		int n = 100000;
+		System.out.println(calc_fib(n));
+	}
+}
