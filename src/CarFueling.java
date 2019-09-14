4564484c2622a8bb9230ac25d011defe23fbf33d
bryanliujh <bbx54ever@hotmail.com>
2019-09-14 19:18:01 +0800

    car_fueling

--- src/CarFueling.java
+++ src/CarFueling.java
@@ -3,62 +3,65 @@ import java.io.*;
 
 public class CarFueling {
     static int computeMinRefills(int dist, int tank, int[] stops) {
-    	int current_dist = 0;
-    	int estimated_dist;
-    	int num_refill = 0;
-    	int current_refill = 0;
-    	int last_refill;
-    	int distance_travelled = 0;
-    	int n = stops.length;
     	
-    	System.out.println(n);
+    	int dist_travelled = 0;
+    	int n = stops.length;
+    	int i = 0;
+    	int num_refill = 0;
     	
     	if (tank >= dist) {
     		return 0;
     	}
     	
-    	while (current_refill <= n-5) {
-    		last_refill = current_refill;
-    		while(current_refill <=n && (stops[current_refill+1] - stops[last_refill] <= dist)) {
-    			current_refill++;
-    		}
-    		
-    		if (current_refill <=n) {
+    	while (dist_travelled + tank < dist) {
+    		if (i<n && stops[i] <= dist_travelled + tank) {
+    			i++;
+    		} else if (i<n) {
+    			dist_travelled = stops[i-1];
     			num_refill++;
+    			if (dist_travelled + tank < stops[i]) {
+    				return -1;
+    			}
+    		} else {
+    			num_refill++;
+    			dist_travelled = stops[i-1];
+    			if (dist_travelled + tank < dist) {
+    				return -1;
+    			}
     		}
     	}
     	
-    	/*
-    	for (int i=0; i<stops.length; i++) {
-    		estimated_dist = current_dist+tank;
-    		if (estimated_dist < stops[i]) {
-    			current_dist += stops[i-1];
-    			if (current_dist+tank < stops[i]) {
-    				return -1;
-    			}
-    			num_refills++;
-    		} 
-    	}*/
+    	
     	
         return num_refill;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        /*
+        
         int dist = scanner.nextInt();
         int tank = scanner.nextInt();
         int n = scanner.nextInt();
         int stops[] = new int[n];
         for (int i = 0; i < n; i++) {
             stops[i] = scanner.nextInt();
-        }*/
-        
+        }
        
+        /*
+        int dist = 10;
+        int tank = 3;
+        int stops[] = {1, 2, 5, 9};*/
         
+        /*
         int dist = 950;
         int tank = 400;
-        int stops[] = {200, 375, 550, 750};
+        int stops[] = {200, 375, 550, 750};*/
+        
+        /*
+        int dist = 700;
+        int tank = 200;
+        int stops[] = {100, 200, 300, 400};*/
+        
 
         System.out.println(computeMinRefills(dist, tank, stops));
     }
