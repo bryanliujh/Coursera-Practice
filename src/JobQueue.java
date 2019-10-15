4002a0e15305637fded2f318abe1ab27e491eb15
bryanliujh <bbx54ever@hotmail.com>
2019-10-16 00:17:26 +0800

    piorityqueue

--- src/JobQueue.java
+++ src/JobQueue.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class JobQueue {
     private int numWorkers;
@@ -8,6 +8,7 @@ public class JobQueue {
     private int[] assignedWorker;
     private long[] startTime;
 
+    private List<WorkerObj> result;
     private FastScanner in;
     private PrintWriter out;
 
@@ -31,21 +32,40 @@ public class JobQueue {
     }
 
     private void assignJobs() {
-        // TODO: replace this code with a faster algorithm.
-        assignedWorker = new int[jobs.length];
+		assignedWorker = new int[jobs.length];
         startTime = new long[jobs.length];
-        long[] nextFreeTime = new long[numWorkers];
-        for (int i = 0; i < jobs.length; i++) {
-            int duration = jobs[i];
-            int bestWorker = 0;
-            for (int j = 0; j < numWorkers; ++j) {
-                if (nextFreeTime[j] < nextFreeTime[bestWorker])
-                    bestWorker = j;
+		PriorityQueue<WorkerObj> pq = new PriorityQueue<WorkerObj>(numWorkers, new Comparator<WorkerObj>(){
+            @Override
+            public int compare (WorkerObj w1, WorkerObj w2) {
+				if(w1.release_time == w2.release_time) {
+					return w1.thread_id - w2.thread_id;
+				}
+				else {
+					return (int) (w1.release_time - w2.release_time);
+				}
             }
-            assignedWorker[i] = bestWorker;
-            startTime[i] = nextFreeTime[bestWorker];
-            nextFreeTime[bestWorker] += duration;
+        });
+		for (int i = 0; i < numWorkers; i++) {
+            pq.offer(new WorkerObj(i));
+		}
+        for (int i = 0; i < jobs.length; i++) {
+        	WorkerObj freeThread = pq.poll();
+            assignedWorker[i] = freeThread.thread_id;
+            startTime[i] = freeThread.release_time;
+            freeThread.release_time += jobs[i];
+            pq.offer(freeThread);
         }
+    }
+    
+    private class WorkerObj {
+    	 int thread_id;
+    	 long release_time;
+    	
+    	public WorkerObj(int thread_id) {
+    		this.thread_id = thread_id;
+    		this.release_time = 0;
+    	}
+    	
     }
 
     public void solve() throws IOException {
